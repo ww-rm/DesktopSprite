65763fc8ed5d39c2f28edc77dc0c3a36a3aab963
ww-rm <ww-rm@qq.com>
2023-05-16 17:03:54 +0800

    add config dialog

--- DesktopSprite/res/resource.h
+++ DesktopSprite/res/resource.h
@@ -6,27 +6,29 @@
 #define IDI_APPICON_LIGHT               102
 #define IDI_TIMEALARM                   103
 #define IDR_CONTEXTMENU                 104
-#define IDD_TRANSPARENCY                105
 #define IDD_ABOUT                       120
 #define IDD_CONFIG                      122
-#define IDC_EDIT_TRANSPARENCY           1003
+#define IDC_APPLY                       1003
 #define IDC_LABELVER                    1004
 #define IDC_PROJECTLINK                 1005
+#define IDC_CHECK_AUTORUN               1006
+#define IDC_CHECK_TIMEALARM             1007
+#define IDC_CHECK_INFOSOUND             1008
+#define IDC_EDIT_BALLOONICONPATH        1009
+#define IDC_BTN_BALLOONICONPATH         1010
+#define IDC_CHECK_FLOATWND              1011
+#define IDC_CHECK_SHOWUSAGE             1012
+#define IDC_CHECK_SHOWNETSPEED          1013
+#define IDC_CHECK_DARKTHEME             1014
+#define IDC_SLIDER_TRANSPARENCY         1015
+#define IDC_STATIC_TRANSPARENCY         1016
 #define IDM_FLOATWND                    40001
 #define IDM_EXIT                        40002
-#define IDM_AUTORUN                     40003
 #define IDM_TIMEALARM                   40004
 #define IDM_INFOSOUND                   40005
-#define IDM_DARKTHEME                   40025
 #define IDM_SHOWCPUMEM                  40026
 #define IDM_SHOWNETSPEED                40027
 #define IDM_TRANSPARENCY                40028
-#define IDM_TRANS_100                   40029
-#define IDM_TRANS_75                    40031
-#define IDM_TRANS_50                    40032
-#define IDM_TRANS_25                    40033
-#define ID__40034                       40034
-#define ID__40035                       40035
 #define IDM_ABOUT                       40036
 #define IDM_CONFIG                      40037
 
@@ -36,7 +38,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        124
 #define _APS_NEXT_COMMAND_VALUE         40038
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
