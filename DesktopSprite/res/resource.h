d092eb60b4c39f8bc811e65ea42d9e3ad0a858be
ww-rm <ww-rm@qq.com>
2023-05-16 15:11:30 +0800

    add about dialog

--- DesktopSprite/res/resource.h
+++ DesktopSprite/res/resource.h
@@ -7,7 +7,11 @@
 #define IDI_TIMEALARM                   103
 #define IDR_CONTEXTMENU                 104
 #define IDD_TRANSPARENCY                105
+#define IDD_ABOUT                       120
+#define IDD_CONFIG                      122
 #define IDC_EDIT_TRANSPARENCY           1003
+#define IDC_LABELVER                    1004
+#define IDC_PROJECTLINK                 1005
 #define IDM_FLOATWND                    40001
 #define IDM_EXIT                        40002
 #define IDM_AUTORUN                     40003
@@ -21,14 +25,18 @@
 #define IDM_TRANS_75                    40031
 #define IDM_TRANS_50                    40032
 #define IDM_TRANS_25                    40033
+#define ID__40034                       40034
+#define ID__40035                       40035
+#define IDM_ABOUT                       40036
+#define IDM_CONFIG                      40037
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        120
-#define _APS_NEXT_COMMAND_VALUE         40034
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_RESOURCE_VALUE        124
+#define _APS_NEXT_COMMAND_VALUE         40038
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
