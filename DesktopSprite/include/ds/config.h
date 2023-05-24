c84d782ec6ff8a73f0103963bcfd49c8080bb578
ww-rm <ww-rm@qq.com>
2023-05-25 01:31:53 +0800

    add spriteconfig

--- DesktopSprite/include/ds/config.h
+++ DesktopSprite/include/ds/config.h
@@ -8,6 +8,25 @@
 #define SHOWCONTENT_CPUMEM              0x1
 #define SHOWCONTENT_NETSPEED            0x2
 
+class SpriteConfig
+{
+public:
+    INT maxFps = 30;
+    WCHAR szSpineAtlasPath[MAX_PATH] = { 0 };
+    WCHAR szSpineSkelPath[MAX_PATH] = { 0 };
+
+    WCHAR spAnimeIdle[MAX_PATH] = L"normal";
+    WCHAR spAnimeDrag[MAX_PATH] = L"tuozhuai";
+    WCHAR spAnimeWork[MAX_PATH] = L"walk";
+    WCHAR spAnimeSleep[MAX_PATH] = L"sleep";
+    WCHAR spAnimeStand[MAX_PATH] = L"stand";
+    WCHAR spAnimeTouch[MAX_PATH] = L"touch";
+    WCHAR spAnimeWink[MAX_PATH] = L"motou";
+    WCHAR spAnimeVictory[MAX_PATH] = L"victory";
+    WCHAR spAnimeDance[MAX_PATH] = L"dance";
+    WCHAR spAnimeDizzy[MAX_PATH] = L"yun";
+};
+
 class AppConfig
 {
 public:
