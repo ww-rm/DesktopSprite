55cfa89ff23e00945371bfba96a2855466173337
ww-rm <ww-rm@qq.com>
2023-05-27 16:17:37 +0800

    update perfmonitor

--- DesktopSprite/include/ds/perfmonitor.h
+++ DesktopSprite/include/ds/perfmonitor.h
@@ -4,58 +4,78 @@
 
 #include <ds/framework.h>
 
-// 性能模块数据
-typedef struct _PERFDATA
-{
-    DOUBLE  cpuPercent;                         // CPU使用百分比
-    DOUBLE  memPercent;                         // 内存使用百分比
-    DOUBLE  uploadSpeed;                        // 上传速度字节
-    DOUBLE  downloadSpeed;                      // 下载速度字节
-}PERFDATA;
+namespace PerfMonitor {
 
-class PerfMonitor
-{
-private:
-    DWORD                   queryInterval = 1000;                  // 查询时间间隔
-    HANDLE                  hQueryEvent = NULL;                    // 控制线程的运行
-    HANDLE                  hQueryThread = NULL;                   // 自动查询线程句柄
-    PERFDATA                perfData = { 0 };                      // 性能数据
-    HANDLE                  hDataMutex = NULL;                     // 用于访问数据的互斥锁
+    // 性能模块数据
+    typedef struct _PERFDATA
+    {
+        DOUBLE  cpuPercent;                         // CPU使用百分比
+        DOUBLE  memPercent;                         // 内存使用百分比
+        DOUBLE  uploadSpeed;                        // 上传速度字节
+        DOUBLE  downloadSpeed;                      // 下载速度字节
+    } PERFDATA;
 
-    PDH_HQUERY              hPdhQuery = NULL;                      // PDH查询句柄
-    PDH_HCOUNTER            hCnterCpu = NULL;                      // cpu计数器
-    PDH_HCOUNTER            hCnterMem = NULL;                      // 内存计数器
-    std::list<PDH_HCOUNTER> hCounterUpSpeedList;                   // 上传计数器列表
-    std::list<PDH_HCOUNTER> hCounterDownSpeedList;                 // 下载计数器列表 
+    class PerfMonitor
+    {
+    private:
+        DWORD                   queryInterval = 1000;                  // 查询时间间隔
+        HANDLE                  hQueryEvent = NULL;                    // 控制线程的运行
+        HANDLE                  hQueryThread = NULL;                   // 自动查询线程句柄
+        PERFDATA                perfData = { 0 };                      // 性能数据
+        HANDLE                  hDataMutex = NULL;                     // 用于访问数据的互斥锁
 
-public:
-    PerfMonitor(DWORD queryInterval = 1000);
-    ~PerfMonitor();
+        PDH_HQUERY              hPdhQuery = NULL;                      // PDH查询句柄
+        PDH_HCOUNTER            hCnterCpu = NULL;                      // cpu计数器
+        PDH_HCOUNTER            hCnterMem = NULL;                      // 内存计数器
+        std::list<PDH_HCOUNTER> hCounterUpSpeedList;                   // 上传计数器列表
+        std::list<PDH_HCOUNTER> hCounterDownSpeedList;                 // 下载计数器列表 
 
-    // 启动监视器
-    BOOL Start();
+        std::map<HWND, INT>     registeredMsg;                         // 已注册的窗口和对应的通知消息
 
-    // 停止监视器
-    BOOL Stop();
+    public:
+        PerfMonitor(DWORD queryInterval = 1000);
+        ~PerfMonitor();
 
-    // 获得性能数据
+        // 启动监视器
+        BOOL Start();
+
+        // 停止监视器
+        BOOL Stop();
+
+        // 注册更新消息
+        BOOL RegisterMessage(HWND hWnd, UINT wndMsg);
+
+        // 获得性能数据
+        BOOL GetPerfData(PERFDATA* pPerfData);
+
+    private:
+        // 线程函数
+        static DWORD CALLBACK QueryThreadProc(_In_ LPVOID lpParameter);
+
+        DWORD QueryThread();
+
+        // 打开查询
+        BOOL OpenQuery();
+
+        // 添加计数器
+        BOOL AddCounter();
+
+        // 开启线程
+        BOOL StartThread();
+
+        // 自动更新函数, 使用子线程调用
+        BOOL QueryPerfData();
+    };
+
+    // global functions
+
+    BOOL InitializePerfMonitor(DWORD queryInterval = 1000);
+
+    BOOL UninitializePerfMonitor();
+
+    BOOL RegisterMessage(HWND hWnd, UINT wndMsg);
+
     BOOL GetPerfData(PERFDATA* pPerfData);
-
-private:
-    // 线程函数
-    static DWORD CALLBACK QueryThreadProc(_In_ LPVOID lpParameter);
-
-    // 打开查询
-    BOOL OpenQuery();
-
-    // 添加计数器
-    BOOL AddCounter();
-
-    // 开启线程
-    BOOL StartThread();
-
-    // 自动更新函数, 使用子线程调用
-    BOOL QueryPerfData();
-};
+}
 
 #endif // !DS_PERFMONITOR_H
--- DesktopSprite/src/ds/perfmonitor.cpp
+++ DesktopSprite/src/ds/perfmonitor.cpp
@@ -2,263 +2,319 @@
 #include <ds/utils.h>
 #include <ds/perfmonitor.h>
 
-PerfMonitor::PerfMonitor(DWORD queryInterval): queryInterval(queryInterval)
-{
-    // 创建数据访问互斥锁
-    this->hDataMutex = CreateMutexW(NULL, FALSE, NULL);
-    this->hQueryEvent = CreateEventW(NULL, TRUE, TRUE, NULL);
-    if (!this->hDataMutex || !this->hQueryEvent)
-    {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        exit(EXIT_FAILURE);
-    }
-}
+namespace PerfMonitor {
 
-PerfMonitor::~PerfMonitor()
-{
-    if (!CloseHandle(this->hDataMutex) || !CloseHandle(this->hQueryEvent))
+    PerfMonitor::PerfMonitor(DWORD queryInterval) : queryInterval(queryInterval)
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-    }
-}
-
-BOOL PerfMonitor::Start()
-{
-    return (BOOL)(this->OpenQuery() && this->AddCounter() && this->StartThread());
-}
-
-BOOL PerfMonitor::Stop()
-{
-    SetEvent(this->hQueryEvent);
-    if (WaitForSingleObject(this->hQueryThread, INFINITE) ||
-        !CloseHandle(this->hQueryThread) ||
-        PdhCloseQuery(this->hPdhQuery))
-    {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        return FALSE;
-    }
-    return TRUE;
-}
-
-BOOL PerfMonitor::GetPerfData(PERFDATA* pPerfData)
-{
-    // 获得数据访问锁
-    if (WaitForSingleObject(this->hDataMutex, INFINITE))
-    {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        return FALSE;
+        // 创建数据访问互斥锁
+        this->hDataMutex = CreateMutexW(NULL, FALSE, NULL);
+        this->hQueryEvent = CreateEventW(NULL, TRUE, TRUE, NULL);
+        if (!this->hDataMutex || !this->hQueryEvent)
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    // 拷贝数据
-    pPerfData->cpuPercent = this->perfData.cpuPercent;
-    pPerfData->memPercent = this->perfData.memPercent;
-    pPerfData->uploadSpeed = this->perfData.uploadSpeed;
-    pPerfData->downloadSpeed = this->perfData.downloadSpeed;
-
-    // 释放访问锁
-    if (!ReleaseMutex(this->hDataMutex))
+    PerfMonitor::~PerfMonitor()
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        return FALSE;
+        if (!CloseHandle(this->hDataMutex) || !CloseHandle(this->hQueryEvent))
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+        }
     }
 
-    return TRUE;
-}
-
-BOOL PerfMonitor::OpenQuery()
-{
-    // 刷新缓存
-    DWORD dwTmp = 0;
-    if (PdhEnumObjectsW(NULL, NULL, NULL, &dwTmp, PERF_DETAIL_WIZARD, TRUE) != PDH_MORE_DATA)
+    BOOL PerfMonitor::Start()
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        return FALSE;
+        return (BOOL)(this->OpenQuery() && this->AddCounter() && this->StartThread());
     }
 
-    // 打开一个查询
-    if (PdhOpenQueryW(NULL, (DWORD_PTR)NULL, &this->hPdhQuery))
+    BOOL PerfMonitor::Stop()
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        return FALSE;
+        SetEvent(this->hQueryEvent);
+        if (WaitForSingleObject(this->hQueryThread, INFINITE) ||
+            !CloseHandle(this->hQueryThread) ||
+            PdhCloseQuery(this->hPdhQuery))
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+            return FALSE;
+        }
+        return TRUE;
     }
 
-    return TRUE;
-}
-
-BOOL PerfMonitor::AddCounter()
-{
-    // 添加CPU计数器和内存计数器
-    if (PdhAddCounterW(this->hPdhQuery, L"\\Processor Information(_Total)\\% Processor Utility", (DWORD_PTR)NULL, &this->hCnterCpu) ||
-        PdhAddCounterW(this->hPdhQuery, L"\\Memory\\% Committed Bytes In Use", (DWORD_PTR)NULL, &this->hCnterMem))
+    inline BOOL PerfMonitor::RegisterMessage(HWND hWnd, UINT wndMsg)
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        return FALSE;
+        this->registeredMsg[hWnd] = wndMsg;
+        return TRUE;
     }
 
-    // cache buffer
-    PDH_HCOUNTER hCounter = NULL;
-    PWSTR pwsCounterListBuffer = NULL;
-    DWORD dwCounterListSize = 0;
-    PWSTR pwsInstanceListBuffer = NULL;
-    DWORD dwInstanceListSize = 0;
-
-    PWSTR pwsTmp = NULL;
-    size_t pwsTmpLen = 0;
-    WCHAR pwsCnterPath[PDH_MAX_COUNTER_PATH];
-
-    BOOL ret = FALSE; // 用作返回值
-
-    // 获取缓冲区大小
-    if (PdhEnumObjectItemsW(
-        NULL, NULL, L"Network Interface",
-        NULL, &dwCounterListSize,
-        NULL, &dwInstanceListSize,
-        PERF_DETAIL_WIZARD, 0
-    ) != PDH_MORE_DATA)
+    BOOL PerfMonitor::GetPerfData(PERFDATA* pPerfData)
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        return FALSE;
+        // 获得数据访问锁
+        if (WaitForSingleObject(this->hDataMutex, INFINITE))
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+            return FALSE;
+        }
+
+        // 拷贝数据
+        pPerfData->cpuPercent = this->perfData.cpuPercent;
+        pPerfData->memPercent = this->perfData.memPercent;
+        pPerfData->uploadSpeed = this->perfData.uploadSpeed;
+        pPerfData->downloadSpeed = this->perfData.downloadSpeed;
+
+        // 释放访问锁
+        if (!ReleaseMutex(this->hDataMutex))
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+            return FALSE;
+        }
+
+        return TRUE;
     }
 
-    // allocate buffer
-    pwsCounterListBuffer = new WCHAR[dwCounterListSize];
-    pwsInstanceListBuffer = new WCHAR[dwInstanceListSize];
-
-    // 获得所有的实例名称
-    if (PdhEnumObjectItemsW(
-        NULL, NULL, L"Network Interface",
-        pwsCounterListBuffer, &dwCounterListSize,
-        pwsInstanceListBuffer, &dwInstanceListSize,
-        PERF_DETAIL_WIZARD, 0
-    ))
+    BOOL PerfMonitor::OpenQuery()
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        goto RETURN_FALSE;
+        // 刷新缓存
+        DWORD dwTmp = 0;
+        if (PdhEnumObjectsW(NULL, NULL, NULL, &dwTmp, PERF_DETAIL_WIZARD, TRUE) != PDH_MORE_DATA)
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+            return FALSE;
+        }
+
+        // 打开一个查询
+        if (PdhOpenQueryW(NULL, (DWORD_PTR)NULL, &this->hPdhQuery))
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+            return FALSE;
+        }
+
+        return TRUE;
     }
 
-    // 添加每一个实例的上传/下载计数器
-    pwsTmp = pwsInstanceListBuffer;
-    while (*pwsTmp != (WCHAR)NULL)
+    BOOL PerfMonitor::AddCounter()
     {
-        OutputDebugStringW(pwsTmp);
-        OutputDebugStringW(L"\n");
+        // 添加CPU计数器和内存计数器
+        if (PdhAddCounterW(this->hPdhQuery, L"\\Processor Information(_Total)\\% Processor Utility", (DWORD_PTR)NULL, &this->hCnterCpu) ||
+            PdhAddCounterW(this->hPdhQuery, L"\\Memory\\% Committed Bytes In Use", (DWORD_PTR)NULL, &this->hCnterMem))
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+            return FALSE;
+        }
 
-        if (FAILED(StringCchPrintfW(pwsCnterPath, PDH_MAX_COUNTER_PATH, L"\\Network Interface(%s)\\Bytes Sent/sec", pwsTmp)) ||
-            PdhAddCounterW(this->hPdhQuery, pwsCnterPath, (DWORD_PTR)NULL, &hCounter))
+        // cache buffer
+        PDH_HCOUNTER hCounter = NULL;
+        PWSTR pwsCounterListBuffer = NULL;
+        DWORD dwCounterListSize = 0;
+        PWSTR pwsInstanceListBuffer = NULL;
+        DWORD dwInstanceListSize = 0;
+
+        PWSTR pwsTmp = NULL;
+        size_t pwsTmpLen = 0;
+        WCHAR pwsCnterPath[PDH_MAX_COUNTER_PATH];
+
+        BOOL ret = FALSE; // 用作返回值
+
+        // 获取缓冲区大小
+        if (PdhEnumObjectItemsW(
+            NULL, NULL, L"Network Interface",
+            NULL, &dwCounterListSize,
+            NULL, &dwInstanceListSize,
+            PERF_DETAIL_WIZARD, 0
+        ) != PDH_MORE_DATA)
+        {
+            ShowLastError(__FUNCTIONW__, __LINE__);
+            return FALSE;
+        }
+
+        // allocate buffer
+        pwsCounterListBuffer = new WCHAR[dwCounterListSize];
+        pwsInstanceListBuffer = new WCHAR[dwInstanceListSize];
+
+        // 获得所有的实例名称
+        if (PdhEnumObjectItemsW(
+            NULL, NULL, L"Network Interface",
+            pwsCounterListBuffer, &dwCounterListSize,
+            pwsInstanceListBuffer, &dwInstanceListSize,
+            PERF_DETAIL_WIZARD, 0
+        ))
         {
             ShowLastError(__FUNCTIONW__, __LINE__);
             goto RETURN_FALSE;
         }
-        this->hCounterUpSpeedList.push_back(hCounter);
 
-        if (FAILED(StringCchPrintfW(pwsCnterPath, PDH_MAX_COUNTER_PATH, L"\\Network Interface(%s)\\Bytes Received/sec", pwsTmp)) ||
-            PdhAddCounterW(this->hPdhQuery, pwsCnterPath, (DWORD_PTR)NULL, &hCounter))
+        // 添加每一个实例的上传/下载计数器
+        pwsTmp = pwsInstanceListBuffer;
+        while (*pwsTmp != (WCHAR)NULL)
+        {
+            OutputDebugStringW(pwsTmp);
+            OutputDebugStringW(L"\n");
+
+            if (FAILED(StringCchPrintfW(pwsCnterPath, PDH_MAX_COUNTER_PATH, L"\\Network Interface(%s)\\Bytes Sent/sec", pwsTmp)) ||
+                PdhAddCounterW(this->hPdhQuery, pwsCnterPath, (DWORD_PTR)NULL, &hCounter))
+            {
+                ShowLastError(__FUNCTIONW__, __LINE__);
+                goto RETURN_FALSE;
+            }
+            this->hCounterUpSpeedList.push_back(hCounter);
+
+            if (FAILED(StringCchPrintfW(pwsCnterPath, PDH_MAX_COUNTER_PATH, L"\\Network Interface(%s)\\Bytes Received/sec", pwsTmp)) ||
+                PdhAddCounterW(this->hPdhQuery, pwsCnterPath, (DWORD_PTR)NULL, &hCounter))
+            {
+                ShowLastError(__FUNCTIONW__, __LINE__);
+                goto RETURN_FALSE;
+            }
+            this->hCounterDownSpeedList.push_back(hCounter);
+
+            if (FAILED(StringCchLengthW(pwsTmp, PDH_MAX_INSTANCE_NAME, &pwsTmpLen)))
+            {
+                ShowLastError(__FUNCTIONW__, __LINE__);
+                goto RETURN_FALSE;
+            }
+            pwsTmp += (pwsTmpLen + 1);
+        }
+
+
+        ret = TRUE; // 如果顺序执行到这里则是成功, 返回 TRUE
+    RETURN_FALSE:
+        delete[] pwsCounterListBuffer;
+        delete[] pwsInstanceListBuffer;
+        return ret;
+    }
+
+    BOOL PerfMonitor::StartThread()
+    {
+        // 打开子线程自动获取数据
+        ResetEvent(this->hQueryEvent);
+        this->hQueryThread = DefCreateThread(PerfMonitor::QueryThreadProc, (LPVOID)this);
+        if (!this->hQueryThread)
         {
             ShowLastError(__FUNCTIONW__, __LINE__);
-            goto RETURN_FALSE;
+            return FALSE;
         }
-        this->hCounterDownSpeedList.push_back(hCounter);
+        return TRUE;
+    }
 
-        if (FAILED(StringCchLengthW(pwsTmp, PDH_MAX_INSTANCE_NAME, &pwsTmpLen)))
+    DWORD CALLBACK PerfMonitor::QueryThreadProc(_In_ LPVOID lpParameter)
+    {
+        return ((PerfMonitor*)lpParameter)->QueryThread();
+    }
+
+    DWORD PerfMonitor::QueryThread()
+    {
+        while (WaitForSingleObject(this->hQueryEvent, 0))
+        {
+            this->QueryPerfData();
+
+            // 发送消息表示已经更新
+            for (auto it = this->registeredMsg.begin(); it != this->registeredMsg.end(); it++)
+            {
+                PostMessageW((*it).first, (*it).second, 0, (LPARAM)this);
+            }
+
+            Sleep(1);
+        }
+        return EXIT_SUCCESS;
+    }
+
+    BOOL PerfMonitor::QueryPerfData()
+    {
+        PDH_FMT_COUNTERVALUE pValue = { 0 };
+
+        // 查询
+        if (PdhCollectQueryData(this->hPdhQuery))
+        {
+            return FALSE;
+        }
+
+        // 间隔一小段时间
+        Sleep(this->queryInterval);
+        if (PdhCollectQueryData(this->hPdhQuery))
+        {
+            return FALSE;
+        }
+
+        // 获得数据访问锁
+        if (WaitForSingleObject(this->hDataMutex, INFINITE))
+        {
+            return FALSE;
+        }
+
+        // 获得CPU数据
+        if (!PdhGetFormattedCounterValue(this->hCnterCpu, PDH_FMT_DOUBLE, NULL, &pValue))
+        {
+            this->perfData.cpuPercent = min(pValue.doubleValue, 100); // 不高于100%
+        }
+
+        // 获得内存数据
+        if (!PdhGetFormattedCounterValue(this->hCnterMem, PDH_FMT_DOUBLE, NULL, &pValue))
+        {
+            this->perfData.memPercent = pValue.doubleValue;
+        }
+
+        // 获得总上传速度
+        this->perfData.uploadSpeed = 0;
+        for (auto item = this->hCounterUpSpeedList.begin(); item != this->hCounterUpSpeedList.end(); item++)
+        {
+            if (!PdhGetFormattedCounterValue(*item, PDH_FMT_DOUBLE | PDH_FMT_NOCAP100, NULL, &pValue))
+            {
+                perfData.uploadSpeed += pValue.doubleValue;
+            }
+        }
+
+        // 获得总下载速度
+        this->perfData.downloadSpeed = 0;
+        for (auto item = this->hCounterDownSpeedList.begin(); item != this->hCounterDownSpeedList.end(); item++)
+        {
+            if (!PdhGetFormattedCounterValue(*item, PDH_FMT_DOUBLE | PDH_FMT_NOCAP100, NULL, &pValue))
+            {
+                perfData.downloadSpeed += pValue.doubleValue;
+            }
+        }
+
+        // 释放访问锁
+        if (!ReleaseMutex(this->hDataMutex))
         {
             ShowLastError(__FUNCTIONW__, __LINE__);
-            goto RETURN_FALSE;
         }
-        pwsTmp += (pwsTmpLen + 1);
+
+        return TRUE;
     }
 
+    // global functions
 
-    ret = TRUE; // 如果顺序执行到这里则是成功, 返回 TRUE
-RETURN_FALSE:
-    delete[] pwsCounterListBuffer;
-    delete[] pwsInstanceListBuffer;
-    return ret;
-}
+    static PerfMonitor* g_perfMonitor = NULL;
 
-BOOL PerfMonitor::StartThread()
-{
-    // 打开子线程自动获取数据
-    ResetEvent(this->hQueryEvent);
-    this->hQueryThread = DefCreateThread(PerfMonitor::QueryThreadProc, (LPVOID)this);
-    if (!this->hQueryThread)
+    BOOL InitializePerfMonitor(DWORD queryInterval)
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
-        return FALSE;
-    }
-    return TRUE;
-}
-
-DWORD CALLBACK PerfMonitor::QueryThreadProc(_In_ LPVOID lpParameter)
-{
-    PerfMonitor* this_ = (PerfMonitor*)lpParameter;
-    while (WaitForSingleObject(this_->hQueryEvent, 0))
-    {
-        this_->QueryPerfData();
-        Sleep(1);
-    }
-    return EXIT_SUCCESS;
-}
-
-BOOL PerfMonitor::QueryPerfData()
-{
-    PDH_FMT_COUNTERVALUE pValue = { 0 };
-
-    // 查询
-    if (PdhCollectQueryData(this->hPdhQuery))
-    {
-        return FALSE;
-    }
-
-    // 间隔一小段时间
-    Sleep(this->queryInterval);
-    if (PdhCollectQueryData(this->hPdhQuery))
-    {
-        return FALSE;
-    }
-
-    // 获得数据访问锁
-    if (WaitForSingleObject(this->hDataMutex, INFINITE))
-    {
-        return FALSE;
-    }
-
-    // 获得CPU数据
-    if (!PdhGetFormattedCounterValue(this->hCnterCpu, PDH_FMT_DOUBLE, NULL, &pValue))
-    {
-        this->perfData.cpuPercent = min(pValue.doubleValue, 100); // 不高于100%
-    }
-
-    // 获得内存数据
-    if (!PdhGetFormattedCounterValue(this->hCnterMem, PDH_FMT_DOUBLE, NULL, &pValue))
-    {
-        this->perfData.memPercent = pValue.doubleValue;
-    }
-
-    // 获得总上传速度
-    this->perfData.uploadSpeed = 0;
-    for (auto item = this->hCounterUpSpeedList.begin(); item != this->hCounterUpSpeedList.end(); item++)
-    {
-        if (!PdhGetFormattedCounterValue(*item, PDH_FMT_DOUBLE | PDH_FMT_NOCAP100, NULL, &pValue))
+        if (!g_perfMonitor)
         {
-            perfData.uploadSpeed += pValue.doubleValue;
+            g_perfMonitor = new PerfMonitor(queryInterval);
+            return g_perfMonitor->Start();
         }
+        return TRUE;
     }
 
-    // 获得总下载速度
-    this->perfData.downloadSpeed = 0;
-    for (auto item = this->hCounterDownSpeedList.begin(); item != this->hCounterDownSpeedList.end(); item++)
+    BOOL UninitializePerfMonitor()
     {
-        if (!PdhGetFormattedCounterValue(*item, PDH_FMT_DOUBLE | PDH_FMT_NOCAP100, NULL, &pValue))
+        if (g_perfMonitor)
         {
-            perfData.downloadSpeed += pValue.doubleValue;
+            g_perfMonitor->Stop();
+            delete g_perfMonitor;
         }
+        return TRUE;
     }
 
-    // 释放访问锁
-    if (!ReleaseMutex(this->hDataMutex))
+    BOOL RegisterMessage(HWND hWnd, UINT wndMsg)
     {
-        ShowLastError(__FUNCTIONW__, __LINE__);
+        if (!g_perfMonitor) return FALSE;
+        return g_perfMonitor->RegisterMessage(hWnd, wndMsg);
     }
 
-    return TRUE;
+    BOOL GetPerfData(PERFDATA* pPerfData)
+    {
+        if (!g_perfMonitor) return FALSE;
+        return g_perfMonitor->GetPerfData(pPerfData);
+    }
 }
